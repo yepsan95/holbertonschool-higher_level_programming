000ad0d5326ae5c5cd5d2f628e1152ba55dcf9dd
Bryan Yep <yepsan95@gmail.com>
2023-05-31 19:38:52 -0700

    fix: shortened code

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -8,7 +8,6 @@ if __name__ == '__main__':
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    op_check = 0
     operators = [["+", add], ["-", sub], ["*", mul], ["/", div]]
     for op in operators:
         if op[0] == argv[2]:
@@ -16,7 +15,6 @@ if __name__ == '__main__':
             a = int(argv[1])
             b = int(argv[3])
             print("{} {} {} = {}".format(a, op[0], b, func(a, b)))
-            op_check = 1
-    if op_check == 0:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+            exit(0)
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
