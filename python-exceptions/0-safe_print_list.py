58bcfb38b6ad6721ef395ae8ce720778c8f129c4
Bryan Yep <yepsan95@gmail.com>
2023-06-11 11:38:20 -0700

    fix: deleted safety conditional

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x <= 0 or not my_list:
-        return 0
     count = 0
     try:
         for i in range(x):
-            print("{}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             count = count + 1
     except Exception:
         print("")
