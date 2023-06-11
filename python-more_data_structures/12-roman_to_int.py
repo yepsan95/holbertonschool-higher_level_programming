80878c47911f78b8827e76a04447bc4e5dbc7898
Bryan Yep <yepsan95@gmail.com>
2023-06-11 13:22:24 -0700

    feat: added function that converts a Roman numeral to an integer

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+            }
+    result = 0
+    i = 0
+    while i < len(roman_string):
+        value = roman[roman_string[i]]
+        if i < len(roman_string) - 1 and value < roman[roman_string[i + 1]]:
+            result = roman[roman_string[i + 1]] - value
+            i += 1
+        else:
+            result += value
+        i += 1
+    return result
