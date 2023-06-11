08a8eafe25a66e7729142264012153bd66d9b6aa
Bryan Yep <yepsan95@gmail.com>
2023-06-11 11:36:36 -0700

    fix: added count variable

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,11 +2,13 @@
 def safe_print_list(my_list=[], x=0):
     if x <= 0 or not my_list:
         return 0
+    count = 0
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
+            count = count + 1
     except Exception:
         print("")
-        return i
+        return count
     print("")
-    return i + 1
+    return count
