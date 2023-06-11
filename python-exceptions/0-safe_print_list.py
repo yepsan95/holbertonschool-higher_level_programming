d2781ae60b4355ab14d1927efe1fc2c9ff2a246e
Bryan Yep <yepsan95@gmail.com>
2023-06-11 11:32:56 -0700

    fix: changed order of for loop and try, except

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,11 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     if x <= 0 or not my_list:
         return 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-        except Exception:
-            print()
-            return i
-    print()
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+    except Exception:
+        print("")
+        return i
+    print("")
     return i + 1
