e3963e5145343b3d9d07d5ce0a8daefaede98882
Bryan Yep <yepsan95@gmail.com>
2023-06-15 16:54:05 -0700

    feat: added Square class definition

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module that defines a square.
+"""
+class Square:
+    """Class square that defines a square.
+    """
+    def __init__(self, size=0):
+        """Constructor '__init__' method.
+
+        Args:
+            __size (int): Size of the square.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """Class 'area' method. Returns the area of the square.
+        """
+        return self.__size ** 2
