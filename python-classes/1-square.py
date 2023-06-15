bf674138dd881e28128ecde2aff24bf04ba4bd58
Bryan Yep <yepsan95@gmail.com>
2023-06-15 16:41:23 -0700

    feat: added definition of class Square

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that defines a square.
+"""
+class Square:
+    """Class square that defines a square.
+    """
+    def __init__(self, size):
+        """Constructor __init__ method.
+
+        Args:
+            __size: Size of the square.
+        """
+        self.__size = size
