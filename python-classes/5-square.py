394b01c202c91dd7dbe8b0b4e748ad6f6f1b3382
Bryan Yep <yepsan95@gmail.com>
2023-06-15 22:16:18 -0700

    feat: added class Square with my_print() method

---
+++ python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Module that defines a square.
+"""
+class Square:
+    """Class square that defines a square.
+    """
+    def __init__(self, size=0):
+        """Constructor '__init__' method.
+
+        Args:
+            __size (int): Size of the square.
+        """
+        self.__size = size
+    @property
+    def size(self):
+        """Getter method.
+        """
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """Class 'size' method. Returns the size of the square.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    def area(self):
+        """Class 'area' method. Returns the area of the square.
+        """
+        return self.__size ** 2
+    def my_print(self):
+        """Class 'my_print' method. Prints the square with the character '#'
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
