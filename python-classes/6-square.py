cd55c792f61c3b6bf09b0119b2ecfd188723bc9f
Bryan Yep <yepsan95@gmail.com>
2023-06-16 08:36:35 -0700

    fix: Y axis of cartesian plane is inverted

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -57,9 +57,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            if self.__position[1] <= 0:
-                for y in range(0, self.__position[1], -1):
-                    print()
+            for y in range(self.__position[1]):
+                print()
             for i in range(self.__size):
                 for x in range(self.__position[0]):
                     print(" ", end="")
