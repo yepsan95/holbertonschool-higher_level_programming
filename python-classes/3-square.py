e8665789f65711f81bba5b1a108cfaa6c5450146
Bryan Yep <yepsan95@gmail.com>
2023-06-16 07:59:30 -0700

    fix: fixed pycode coding style

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Module that defines a square.
 """
+
+
 class Square:
     """Class square that defines a square.
     """
@@ -15,6 +17,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """Class 'area' method. Returns the area of the square.
         """
