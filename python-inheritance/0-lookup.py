8650e78379436eefeea23626f2c6304d2faf2c4b
Bryan Yep <yepsan95@gmail.com>
2023-06-23 16:10:53 -0700

    fix: added documentation for module and function

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+This module defines the 'lookup()' function.
+"""
+
+
 def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+    """
     return dir(obj)
