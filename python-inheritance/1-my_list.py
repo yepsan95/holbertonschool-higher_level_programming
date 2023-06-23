4eb926cdbb878063fcc58bf272fe04f7c9d1b971
Bryan Yep <yepsan95@gmail.com>
2023-06-23 16:53:24 -0700

    feat: added module that defines a class 'MyList'

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines a class 'MyList' that inherits from 'list'.
+
+"""
+
+
+class MyList(list):
+    """
+    Subclass of 'list'.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list, but sorted (ascending order).
+        """
+        print(sorted(self))
