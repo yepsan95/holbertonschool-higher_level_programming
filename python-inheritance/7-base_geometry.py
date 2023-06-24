ef8bc54a23ff202fd60cb40d8745c0e6fb879afb
Bryan Yep <yepsan95@gmail.com>
2023-06-23 20:37:16 -0700

    feat: added method 'integer_validator()'

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This modules defines a class 'BaseGeometry'.
+"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry.
+    """
+    def area(self):
+        """
+        Raises an exception.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates if value is an integer and it's greater than 0.
+
+        Args:
+            name (str): name of the parameter.
+            value (int): value of the parameter.
+        Returns:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".(name))
