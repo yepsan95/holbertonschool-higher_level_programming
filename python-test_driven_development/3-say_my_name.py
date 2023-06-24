de592b1479d69e0c3d9246bf5b4df9bc9bdd62af
Bryan Yep <yepsan95@gmail.com>
2023-06-23 20:55:01 -0700

    fix: added documentation

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""
+This module defines the function 'say_my_name()'.
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Prints the name and last name of a person.
+
+    Args:
+        first_name (str): First name of the person.
+        last_name (str): Last name of the person.
+    """
+
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
