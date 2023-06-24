8d96b98ce5a66d4b5b2de2fa8ea8f950d6152db9
Bryan Yep <yepsan95@gmail.com>
2023-06-23 19:25:21 -0700

    feat: added module with an improved class BaseGeometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This modules defines a class 'BaseGeometry'.
+"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry.
+    """
+    def area(self):
+        """
+        Raises an exception.
+        """
+        raise Exception("area() is not implemented")
