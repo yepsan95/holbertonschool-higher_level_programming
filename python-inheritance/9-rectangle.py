38fc86b2c4eb9376a5f650e9b2b3ae4d3aa3a567
Bryan Yep <yepsan95@gmail.com>
2023-06-25 08:03:30 -0700

    fix: corrected __str__ class method

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module defines the class 'Rectangle' that inherits from 'BaseGeometry'.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Defines a rectangle. Inherits from 'BaseGeometry'.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initializes a new instance of class 'Rectangle'.
+
+        Args:
+            width (int): Width of the rectangle.
+            height (int): Height of the rectangle.
+        """
+
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """
+        Returns the area of the rectangle.
+        """
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Returns a human-readable string representation of the object.
+        """
+
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
