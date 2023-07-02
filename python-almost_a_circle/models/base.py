f0b3f57876b72f84889d4b1c64d0b4fb2edb7f42
Bryan Yep <yepsan95@gmail.com>
2023-07-01 18:36:49 -0700

    feat: added static method to_json_string()

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 This module defines the class 'Base'.
 """
+import json
 
 
 class Base:
@@ -27,3 +28,17 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
